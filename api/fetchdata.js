5ee7c07d9986b2113cc6470069ffa7d8a59250c6
Biresh <workinguse5@gmail.com>
2024-01-27 23:31:02 +0530

    serverless try 1

---
+++ api/fetchdata.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+module.exports = async (req, res) => {
+    let activity = []
+
+    try {
+        const response = await axios.get('https://api.github.com/users/BIlla05/events', {
+            headers: {
+                Authorization: `Bearer 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`
+            }
+        });
+
+        if (response.status < 200 || response.status >= 300) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.data;
+
+        for (let i=0;i<data.length;i++){
+            let date = new Date(data[i]?.created_at);
+            let repo_name = data[i]?.repo?.name;
+            let issue_title = data[i]?.payload?.issue?.title;
+            let commit_msg = data[i]?.payload?.commits?.[0]?.message;
+
+            let existingActivity = activity.find(a => a.issue_title === issue_title);
+
+            if (existingActivity) {
+                let existingDate = new Date(existingActivity.date);
+                if (date > existingDate) {
+                    let index = activity.indexOf(existingActivity);
+                    activity[index] = {
+                        date: date,
+                        repo_name: repo_name,
+                        issue_title: issue_title,
+                        commit_msg: commit_msg
+                    };
+                }
+            } else {
+                activity.push({
+                    date: date,
+                    repo_name: repo_name,
+                    issue_title: issue_title,
+                    commit_msg: commit_msg
+                });
+            }
+        }
+
+        res.json(activity.reverse());
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+};
